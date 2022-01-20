8fe625e8f5b498b598045a18bf1ac7349455b107
José Fernando Höwer Barbosa <61600906+Josehower@users.noreply.github.com>
2022-01-20 16:08:35 +0100

    Add guest prefix to endpoints (#121)

--- index.ts
+++ index.ts
@@ -30,13 +30,20 @@ app.use(function (req, res, next) {
   next();
 });
 
-// Get all guests
+// Get endpoints
 app.get('/', function (req, res) {
+  res.json({
+    guests: `${req.protocol}://${req.get('host')}/guests/`,
+  });
+});
+
+// Get all guests
+app.get('/guests', function (req, res) {
   res.json(guestList);
 });
 
 // New guest
-app.post('/', function (req, res) {
+app.post('/guests', function (req, res) {
   if (!req.body.firstName || !req.body.lastName) {
     res.status(400).json({
       errors: [
@@ -72,7 +79,7 @@ app.post('/', function (req, res) {
 });
 
 // Modify a single guest
-app.patch('/:id', function (req, res) {
+app.patch('/guests/:id', function (req, res) {
   const allowedKeys = ['firstName', 'lastName', 'deadline', 'attending'];
   const difference = Object.keys(req.body).filter(
     (key) => !allowedKeys.includes(key),
@@ -112,7 +119,7 @@ app.patch('/:id', function (req, res) {
 });
 
 // Delete a single guest
-app.delete('/:id', function (req, res) {
+app.delete('/guests/:id', function (req, res) {
   const guest = guestList.find(
     (currentGuest) => currentGuest.id === req.params.id,
   );
